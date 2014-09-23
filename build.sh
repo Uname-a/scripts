172da6d0307c4d453b9af38546365888b7ed5a62
matt baird <billybob2435@gmail.com>
2014-09-23 13:37:30 -0500

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+cd $3
 
-gcc -g -Wall  $1
+arr=( $(find  -maxdepth 1 -type f  -name "*.c" ! -name "$2.*"  ))
+
+echo ${arr[@]}
+build=()
+echo "Other files to build with? enter the name without the './', then press enter:"
+read -a build 
+
+gcc -g -Wall $1 ${build[@]} -o $2
+
+input=()
+echo "enter command line input leave blank if none, then press enter:"
+read -a input
+./$2 ${input[@]}
 
 echo "
 
@@ -9,3 +22,4 @@ echo "
 
 
 read  -p "Hit ENTER to close"
+-maxdepth 1
